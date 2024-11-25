66c354e020067b0534357c03c18b07dbeede8611
JatinShimpi <shimpijatin184@gmail.com>
2024-11-25 23:06:27 +0530

    changed the backend to a different repo

--- ems-backend/src/main/java/com/jatin/ems_backend/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jatin.ems_backend.repository;
-
-import com.jatin.ems_backend.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department,Long> {
-}
--- ems-backend/src/main/java/com/jatin/ems_backend/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jatin.ems_backend.repository;
-
-import com.jatin.ems_backend.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-
-}
